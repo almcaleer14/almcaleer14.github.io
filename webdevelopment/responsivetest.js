e9219d483a7c1739c022943d07ef9b664cf20f53
Alison McAleer <ampdp@umsystem.edu>
2025-07-06 16:08:55 -0500

    Add files via upload

---
+++ webdevelopment/responsivetest.js
@@ -0,0 +1,55 @@
+// Toggle Mobile Menu
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+function toggleMenu(e) {
+  e.preventDefault(); // stops link jump
+  menu.classList.toggle('active'); // show/hide menu
+  const icon = toggle.querySelector('i');
+
+  if (menu.classList.contains('active')) {
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
+  } else {
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+  }
+}
+
+toggle.addEventListener('click', toggleMenu);
+
+// Submenu toggle
+const items = document.querySelectorAll('.item');
+
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else {
+    // close other open submenus
+    items.forEach(item => item.classList.remove("submenu-active"));
+    this.classList.add("submenu-active");
+  }
+}
+
+items.forEach(item => {
+  if (item.querySelector('.submenu')) {
+    item.addEventListener('click', toggleItem);
+    item.addEventListener('keypress', toggleItem);
+  }
+});
+
+const hamburger = document.getElementById('hamburger-icon');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.dropdown > a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      this.parentElement.classList.toggle('open');
+    }
+  });
+});
